7e4617502838a00ee726890a0ee78e3bfab53da8
Haf <haf@protonmail.ch>
2023-01-11 07:48:09 +0100

    stuck trying to get fragments to work

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,16 +1,25 @@
 import { useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
 import { Container } from "react-bootstrap";
+import RepositoryCard, { REPOSITORY_CARD_FRAGMENT } from "../src/components/RepositoryCard";
 import { gql } from "../src/__generated__";
 
-export const USER_QUERY = gql(`
+export const USER_QUERY = gql`
     query GetUserData($login: String!) {
         user(login: $login) {
             bio
             name
+            repositories(first: 10) {
+                edges {
+                    node {
+                        ... RepositoryCard
+                    }
+                }
+            }
         }
     }
-`);
+    ${REPOSITORY_CARD_FRAGMENT}
+`;
 
 const Profile = () => {
     const router = useRouter();
@@ -25,6 +34,15 @@ const Profile = () => {
         <Container>
             <h1 className="display-1">{ data?.user?.name }</h1>
             <p>{data?.user?.bio}</p>
+            {
+                data?.user?.repositories?.edges?.map((repository) => (
+                    <RepositoryCard 
+                        key={repository.resourcePath}
+                        resourcePath={repository.resourcePath} 
+                        description={repository.description}
+                    />
+                ))
+            }
         </Container>
     );
 };
---
+++ src/components/RepositoryCard.tsx
@@ -0,0 +1,30 @@
+import { FC } from "react";
+import Card from "react-bootstrap/Card";
+import { gql } from "../__generated__";
+
+export const REPOSITORY_CARD_FRAGMENT = gql`
+    fragment RepositoryCard on Repository {
+        resourcePath
+        description
+    }
+`; 
+
+type RepositoryCardProps = {
+    readonly resourcePath: string;
+    readonly description: string;
+};
+
+const RepositoryCard: FC<RepositoryCardProps> = ({ resourcePath, description }) => {
+    return (
+        <Card>
+            <Card.Body>
+                <Card.Title>{resourcePath}</Card.Title>
+                <Card.Text>
+                    {description}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default RepositoryCard;
